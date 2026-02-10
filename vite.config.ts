faba1e867c837a5752a94b72ecd546201953faad
AkirahaWeb <137966748+AkirahaWeb@users.noreply.github.com>
2026-02-10 17:17:35 -0300

    Refactor Vite config and add build settings
    Refactor Vite configuration to improve structure and add build options.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import path from 'path';
-import { defineConfig } from 'vite'; // Removi o loadEnv daqui
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig(() => {
-    return {
-      base: './', 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  // ESSENCIAL: O ponto antes da barra garante que o App ache os próprios arquivos no Itch.io
+  base: './', 
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    // Isso ajuda a evitar erros de caminhos em arquivos muito grandes
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 });
